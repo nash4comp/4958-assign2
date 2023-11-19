f656e13321f0a722ce6fa36d4ecdf5b04ccee753
NB <nash4comp@gmail.com>
2023-11-18 22:10:44 -0800

    First commit

---
+++ client/Main.java
@@ -0,0 +1,54 @@
+package client;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 6666;
+
+        if (args.length >= 2) {
+            host = args[0];
+            port = Integer.parseInt(args[1]);
+        } else if (args.length == 1) {
+            host = args[0];
+        }
+
+        try (Socket socket = new Socket(host, port);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                Scanner userInput = new Scanner(System.in)) {
+
+            System.out.println("Connected to the Go chat server");
+
+            Thread responseThread = new Thread(() -> {
+                try {
+                    while (true) {
+                        String response = in.readLine();
+                        if (response != null) {
+                            System.out.println("=> " + response);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            responseThread.start();
+
+            while (true) {
+                String userCommand = userInput.nextLine();
+                if (userCommand == null) {
+                    break;
+                }
+
+                out.write(userCommand);
+                out.newLine();
+                out.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
